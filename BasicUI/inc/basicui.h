95c619ac03f41d50f16871adc4a9c55f8962869c
donghan <tnqkr98@naver.com>
2021-01-29 20:42:38 +0900

    Study Tizen

---
+++ BasicUI/inc/basicui.h
@@ -0,0 +1,19 @@
+#ifndef __basicui_H__
+#define __basicui_H__
+
+#include <app.h>
+#include <Elementary.h>
+#include <system_settings.h>
+#include <efl_extension.h>
+#include <dlog.h>
+
+#ifdef  LOG_TAG
+#undef  LOG_TAG
+#endif
+#define LOG_TAG "basicui"
+
+#if !defined(PACKAGE)
+#define PACKAGE "org.example.basicui"
+#endif
+
+#endif /* __basicui_H__ */
---
+++ gosleepnyx/inc/gosleepnyx.h
@@ -0,0 +1,19 @@
+#ifndef __gosleepnyx_H__
+#define __gosleepnyx_H__
+
+#include <app.h>
+#include <Elementary.h>
+#include <system_settings.h>
+#include <efl_extension.h>
+#include <dlog.h>
+
+#ifdef  LOG_TAG
+#undef  LOG_TAG
+#endif
+#define LOG_TAG "gosleepnyx"
+
+#if !defined(PACKAGE)
+#define PACKAGE "org.example.gosleepnyx"
+#endif
+
+#endif /* __gosleepnyx_H__ */
